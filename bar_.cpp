99e4156f8acd3a8a19142a9b6dbcd7dd74e86fa2
Andrey <68009565+andreystrokov@users.noreply.github.com>
2021-07-24 12:23:13 +0300

    bar_ 2.0
    Most supporting decision

--- bar_.cpp
+++ bar_.cpp
@@ -3,49 +3,73 @@
 
 using namespace std;
 
-    struct Ingr {
-        string name;
-        int num;
-        char symbol;
-    };
-int main() {
-    size_t h,w = 0;
-    cin >> h >> w;
-    cin.ignore(1);
-    vector<string> cup(h);
-    for (int i = --h;i >= 0; --i){
-        string s;
-        getline(cin,s);
-        cup[i] = s;
-    }
-    int ingr;
-    cin >> ingr;
-    vector<Ingr> v;
-    while (ingr){
-        string name;
-        cin >> name;
-        int num;
-        cin >> num;
-        char c;
-        cin >> c;
-        v.push_back({name,num,c});
-        --ingr;
-    }
-    int current_string = 1;
-        for(auto& j : v){
-            for (int i = current_string; i < current_string + j.num; ++i){
-                for (char& c : cup[i]){
-                    if (c == ' '){
-                        c = j.symbol;
-                    }
-                }
-            }
-        current_string +=j.num;
+struct TCup {
+    vector<string> Levels;
+    int CurrentLevel;
+
+    void Read() {
+        size_t height, width = 0;
+        cin >> height >> width;
+        cin.ignore(1);
+        Levels.resize(height);
+        string level;
+        for (int i = 0; i < height; ++i) {
+            getline(cin, level);
+            Levels[i] = level;
         }
-
-    for (int i = h;i >= 0; --i){
-        cout << cup[i] << endl;
+        CurrentLevel = height - 1;
     }
 
-  return 0;
+    void Fill(char symbol) {
+        auto& level = Levels[--CurrentLevel];
+
+        int start = level.find(' ');
+        if (start == string::npos) {
+            return;
+        }
+        int finish = level.rfind(' ');
+        for (int i = start; i <= finish; ++i) {
+            level[i] = symbol;
+        }
+    }
+
+    void Draw() {
+        for (const auto& level : Levels) {
+            cout << level << endl;
+        }
+    }
+};
+
+struct TIngredient {
+    string Name;
+    int LayersCount;
+    char Symbol;
+
+    void Read() {
+        cin >> Name;
+        cin >> LayersCount;
+        cin >> Symbol;
+    }
+
+    void Fill(TCup& cup) {
+        for (int i = 0; i < LayersCount; ++i) {
+            cup.Fill(Symbol);
+        }
+    }
+};
+
+int main() {
+    TCup cup;
+    cup.Read();
+
+    int ingredientsCount;
+    cin >> ingredientsCount;
+    
+    TIngredient ingredient;
+    for (int i = 0; i < ingredientsCount; ++i) {
+        ingredient.Read();
+        ingredient.Fill(cup);
+    }
+    cup.Draw();
+    return 0;
 }
